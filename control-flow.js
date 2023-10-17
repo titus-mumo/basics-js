bd4b7b4334b7ece8977b2f09dc0f051653cf5e2d
titus-mumo <tituskennedy74@gmail.com>
2023-10-17 22:50:04 +0300

    Just code

--- control-flow.js
+++ control-flow.js
@@ -4,3 +4,13 @@ let students = ['jimmy', 'alex', 'mark', 'deepak']
 for(let i = 0; i < students.length; i++){
     console.log(students[i])
 }
+
+//in & off
+
+for(let student of students){//we have access to the value, access empty value
+    console.log(student)
+}
+
+for (let index in students){//with in, we have access to index/key, skips empty value
+    console.log(students[index])
+}
