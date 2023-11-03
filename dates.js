cecf02137779a2687a852b27850d3e98f5d36bf1
titus-mumo <tituskennedy74@gmail.com>
2023-11-03 10:54:02 +0300

    Just code

---
+++ dates.js
@@ -0,0 +1,22 @@
+const now = new Date()
+
+console.log(now)
+console.log(typeof now)
+console.log(now.getFullYear())
+console.log(now.getMonth())
+console.log(now.getDate())
+console.log(now.getDay())
+console.log(now.getHours())
+console.log(now.getMinutes())
+console.log(now.getSeconds())
+console.log(now.toDateString())
+console.log(now.toTimeString())
+console.log(now.toLocaleDateString())
+console.log(now.toLocaleString())
+console.log(now.toLocaleTimeString())
+
+// time stamps
+console.log(now.getTime())
+const current = new Date()
+console.log(current.getTime() - now.getTime())
+console.log(current-now)
